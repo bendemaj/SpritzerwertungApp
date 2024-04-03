751afa42d24c74430d93b9c851ff308902c126ba
Ben Demaj <bendemaj.ad@gmail.com>
2024-04-03 10:04:21 +0200

    initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,281 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { Plus, RotateCcw, RotateCw } from "lucide-react";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+let initalTeams = [
+  { id: 0, name: "Hansi", count: 0 },
+  { id: 1, name: "Felix", count: 0 },
+  { id: 2, name: "Nowak", count: 0 },
+  { id: 3, name: "Ben", count: 0 },
+];
+
+let nextId = 0;
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+  const [team, setTeam] = useState<
+    { id: number; name: string; count: number }[]
+  >(JSON.parse(localStorage.getItem("team") || "[]"));
+
+  const incrementCount = (id: number, incrementValue: number) => {
+    setCounter((cnt) => cnt + incrementValue);
+  };
+
+  const handleSubmit = (id: number) => {
+    setTeam((prevTeams) =>
+      prevTeams.map((team) =>
+        team.id === id ? { ...team, count: team.count + counter } : team
+      )
+    );
+    setCounter(0);
+    sortTeamsByCount();
+  };
+
+  const sortTeamsByCount = () => {
+    setTeam((prevTeams) => [...prevTeams].sort((a, b) => b.count - a.count));
+  };
+
+  const getTeamIndex = () => {
+    return team
+      .map((team, index) => ({
+        index: index + 1,
+        team: team,
+      }))
+      .sort((a, b) => b.team.count - a.team.count);
+  };
+
+  const getMedalEmoji = (index: number, count: number) => {
+    if (index === 0 && count > 0) {
+      return <span>&#127942;</span>;
+    }
+  };
+
+  useEffect(() => {
+    console.log(getTeamIndex());
+  });
+
+  useEffect(() => {
+    localStorage.setItem("team", JSON.stringify(team));
+  }, [team]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <Tabs defaultValue="list" className="w-max-md">
+        <TabsList>
+          <TabsTrigger value="list">Liste</TabsTrigger>
+          <TabsTrigger value="graphic">Verlauf</TabsTrigger>
+        </TabsList>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <TabsContent value="list">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Team</TableHead>
+                <TableHead className="text-right">Menge</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {getTeamIndex().map((teamData, index) => (
+                <TableRow key={teamData.team.id}>
+                  <TableCell className="w-[100px]">
+                    <Drawer>
+                      <DrawerTrigger>
+                        {getMedalEmoji(index, teamData.team.count)}
+                        {teamData.team.name}
+                      </DrawerTrigger>
+                      <DrawerContent>
+                        <div className="mx-auto w-full max-w-sm">
+                          <DrawerHeader>
+                            <DrawerTitle>
+                              {`Team ` + teamData.team.name + ` löschen`}
+                            </DrawerTitle>
+                            <DrawerDescription>
+                              Willst du wirklich dieses Team löschen?
+                            </DrawerDescription>
+                          </DrawerHeader>
+                          <DrawerFooter>
+                            <DrawerClose className="grid gap-2">
+                              <Button
+                                variant="destructive"
+                                onClick={() => {
+                                  setTeam(
+                                    team.filter(
+                                      (team) => team.id !== teamData.team.id
+                                    )
+                                  );
+                                }}
+                              >
+                                Löschen
+                              </Button>
+                              <Button>Abbruch</Button>
+                            </DrawerClose>
+                          </DrawerFooter>
+                        </div>
+                      </DrawerContent>
+                    </Drawer>
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Drawer>
+                      <DrawerTrigger>{teamData.team.count}</DrawerTrigger>
+                      <DrawerContent>
+                        <div className="mx-auto w-full max-w-sm">
+                          <DrawerHeader>
+                            <DrawerTitle>Spritzer🍇</DrawerTitle>
+                            <DrawerDescription>
+                              Anzahl an verkauften Spritzer eintragen...
+                            </DrawerDescription>
+                          </DrawerHeader>
+                          <div className="flex-1 text-center">
+                            <div className="text-7xl font-bold tracking-tighter">
+                              {counter}
+                            </div>
+                            <div className="text-[0.70rem] uppercase text-muted-foreground">
+                              Spritzer
+                            </div>
+                          </div>
+                          <div className="p-4 flex justify-between">
+                            <Button
+                              variant="outline"
+                              size="icon"
+                              className="h-10 w-10 shrink-0 rounded-full"
+                              onClick={() =>
+                                incrementCount(teamData.team.id, 1)
+                              }
+                            >
+                              +1
+                            </Button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                            <Button
+                              variant="outline"
+                              size="icon"
+                              className="h-10 w-10 shrink-0 rounded-full"
+                              onClick={() =>
+                                incrementCount(teamData.team.id, 2)
+                              }
+                            >
+                              +2
+                            </Button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                            <Button
+                              variant="outline"
+                              size="icon"
+                              className="h-10 w-10 shrink-0 rounded-full"
+                              onClick={() =>
+                                incrementCount(teamData.team.id, 10)
+                              }
+                            >
+                              +10
+                            </Button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                            <Button
+                              variant="outline"
+                              size="icon"
+                              className="h-10 w-10 shrink-0 rounded-full"
+                              onClick={() => setCounter(0)}
+                            >
+                              <RotateCcw className="h-5 w-5" />
+                            </Button>
+                          </div>
+                          <DrawerFooter>
+                            <DrawerClose asChild>
+                              <Button
+                                onClick={() => handleSubmit(teamData.team.id)}
+                              >
+                                Submit
+                              </Button>
+                            </DrawerClose>
+                          </DrawerFooter>
+                        </div>
+                      </DrawerContent>
+                    </Drawer>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TabsContent>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <TabsContent value="graphic"></TabsContent>
+      </Tabs>
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="secondary" className="mt-5">
+            <Plus className="mr-2 h-4 w-4" /> Neues Team
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Neues Team erstellen</AlertDialogTitle>
+            <AlertDialogDescription>
+              <div className="grid w-full max-w items-center gap-1.5">
+                <Label htmlFor="name">Team</Label>
+                <Input onChange={(e) => setName(e.target.value)} />
+              </div>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Abbruch</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                setTeam([
+                  ...team,
+                  { id: nextId++, name: name, count: counter },
+                ]);
+              }}
+            >
+              Speichern
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Button
+        variant="outline"
+        size="icon"
+        className="h-10 w-10 shrink-0 rounded-full ml-2"
+        onClick={() => window.location.reload()}
+      >
+        <RotateCw className="h-5 w-5" />
+      </Button>
+      
     </main>
   );
 }
